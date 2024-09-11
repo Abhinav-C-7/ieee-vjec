ab0324f5cd85fc163f23126e3d36865d93206410
Abhinav-C-7 <abhinavc038@gmail.com>
2024-09-11 14:47:55 +0530

    first commit

---
+++ src/assets/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './navbar.css';
+
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <nav >
+        <ul className='flex justify-center mt-4' >
+            <li>
+                <img src="" alt="" />
+
+            </li>
+            <li>Execom</li>
+            <li>Achievements</li>
+            <li>Gallery</li>
+            <li>Join IEEE</li>
+            <li>About</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
